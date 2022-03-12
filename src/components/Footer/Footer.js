0f3d91f9e5f375afb472d69390b7ac87bacf0bd7
Rebecca Toek <rebbytoek095@gmail.com>
2022-03-12 13:21:59 +0300

    Changed name

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
           <Slogan>&copy; 2021 <Link href="https://www.vipuljha.com">  Rebecca Cheptoek</Link> . All rights reserved.</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://github.com/lordarcadius">
+          <SocialIcons href="https://github.com/Rebeccacheptoek/">
             <AiFillGithub size="3rem" />
           </SocialIcons>
           <SocialIcons href="https://www.linkedin.com/in/lordarcadius/">
